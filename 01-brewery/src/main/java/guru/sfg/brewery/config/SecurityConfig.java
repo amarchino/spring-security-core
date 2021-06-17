529da92d375d8c2a5ebee8556d305dd713038174
amarchino <marchino.alessandro@gmail.com>
2021-06-17 23:17:59 +0200

    Lesson 25

--- 01-brewery/src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ 01-brewery/src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package guru.sfg.brewery.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,10 +12,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests(requests -> {
-			requests.antMatchers("/", "/webjars/**", "/resources/**").permitAll();
-			requests.antMatchers("/beers/find", "/beers*").permitAll();
-		});
+		http.authorizeRequests(requests ->
+			requests.antMatchers("/", "/webjars/**", "/resources/**").permitAll()
+				.antMatchers("/beers/find", "/beers*").permitAll()
+				.antMatchers(HttpMethod.GET, "/api/v1/beer/**").permitAll()
+		);
 		http.authorizeRequests(requests -> requests.anyRequest().authenticated());
 		http.formLogin();
 		http.httpBasic();
---
+++ 01-brewery/src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
@@ -0,0 +1,27 @@
+package guru.sfg.brewery.web.controllers.api;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import guru.sfg.brewery.web.controllers.BaseIT;
+
+@WebMvcTest
+public class BeerRestControllerIT extends BaseIT {
+	
+	@Test
+	void findBeers() throws Exception {
+		mockMvc.perform(get("/api/v1/beer"))
+			.andExpect(status().isOk());
+	}
+
+	@Test
+	void findBeerById() throws Exception {
+		mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID()))
+			.andExpect(status().isOk());
+	}
+}

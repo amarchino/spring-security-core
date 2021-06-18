3c3073a93208a1c56173b194a8c6f6bde5b906d4
amarchino <marchino.alessandro@gmail.com>
2021-06-18 23:39:05 +0200

    Assignment 02

--- 01-brewery/src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
+++ 01-brewery/src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
@@ -30,6 +30,14 @@ public class BeerControllerIT extends BaseIT {
             .andExpect(view().name("beers/createBeer"))
             .andExpect(model().attributeExists("beer"));
     }
+	
+	@Test
+    void initCreationFormScottTiger() throws Exception{
+    	mockMvc.perform(get("/beers/new").with(httpBasic("scott", "tiger")))
+            .andExpect(status().isOk())
+            .andExpect(view().name("beers/createBeer"))
+            .andExpect(model().attributeExists("beer"));
+    }
 
     @Test
     void findBeers() throws Exception{

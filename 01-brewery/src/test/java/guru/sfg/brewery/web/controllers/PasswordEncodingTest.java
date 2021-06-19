72389e432af43da847d0afb0f7d0e150194a5b8a
amarchino <marchino.alessandro@gmail.com>
2021-06-19 15:23:31 +0200

    Lesson 35

--- 01-brewery/src/test/java/guru/sfg/brewery/web/controllers/PasswordEncodingTest.java
+++ 01-brewery/src/test/java/guru/sfg/brewery/web/controllers/PasswordEncodingTest.java
@@ -1,8 +1,10 @@
 package guru.sfg.brewery.web.controllers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.util.DigestUtils;
@@ -12,17 +14,28 @@ public class PasswordEncodingTest {
 	
 	@Test
 	void hashingExample() {
-		System.out.println(DigestUtils.md5DigestAsHex(PASSWORD.getBytes()));
-		System.out.println(DigestUtils.md5DigestAsHex(PASSWORD.getBytes()));
+		System.out.println("hashingExample: " + DigestUtils.md5DigestAsHex(PASSWORD.getBytes()));
+		System.out.println("hashingExample: " + DigestUtils.md5DigestAsHex(PASSWORD.getBytes()));
 		
 		String salted = PASSWORD + "ThisIsMySALTVALUE";
-		System.out.println(DigestUtils.md5DigestAsHex(salted.getBytes()));
+		System.out.println("hashingExample: " + DigestUtils.md5DigestAsHex(salted.getBytes()));
 	}
 	
 	@Test
 	void testNoOp() {
 		PasswordEncoder noOp = NoOpPasswordEncoder.getInstance();
-		System.out.println(noOp.encode(PASSWORD));
-		assertEquals(noOp.encode(PASSWORD), PASSWORD);
+		String encodedPassword = noOp.encode(PASSWORD);
+		System.out.println("testNoOp: " + encodedPassword);
+		assertTrue(noOp.matches(PASSWORD, encodedPassword));
+	}
+	
+	@Test
+	void testLdap() {
+		PasswordEncoder ldap = new LdapShaPasswordEncoder();
+		System.out.println("testLdap: " + ldap.encode(PASSWORD));
+		String encodedPassword = ldap.encode(PASSWORD);
+		System.out.println("testLdap: " + encodedPassword);
+		
+		assertTrue(ldap.matches(PASSWORD, encodedPassword));
 	}
 }

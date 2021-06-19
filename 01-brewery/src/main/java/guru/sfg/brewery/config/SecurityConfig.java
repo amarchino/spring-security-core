2afaed16fd88a9bd5da6d42ebec305075f796b90
amarchino <marchino.alessandro@gmail.com>
2021-06-19 15:15:51 +0200

    Lesson 34

--- 01-brewery/src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ 01-brewery/src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package guru.sfg.brewery.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -28,13 +31,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
 			.withUser("spring")
-			.password("{noop}guru")
+			.password("guru")
 			.roles("ADMIN")
 			.and()
 			.withUser("user")
-			.password("{noop}password")
+			.password("password")
 			.roles("USER");
-		auth.inMemoryAuthentication().withUser("scott").password("{noop}tiger").roles("CUSTOMER");
+		auth.inMemoryAuthentication().withUser("scott").password("tiger").roles("CUSTOMER");
 	}
 	
 //	@Override
@@ -55,6 +58,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //		return new InMemoryUserDetailsManager(admin, user);
 //	}
 	
-	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
 	
 }
--- 01-brewery/src/test/java/guru/sfg/brewery/web/controllers/PasswordEncodingTest.java
+++ 01-brewery/src/test/java/guru/sfg/brewery/web/controllers/PasswordEncodingTest.java
@@ -1,6 +1,10 @@
 package guru.sfg.brewery.web.controllers;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.util.DigestUtils;
 
 public class PasswordEncodingTest {
@@ -13,5 +17,12 @@ public class PasswordEncodingTest {
 		
 		String salted = PASSWORD + "ThisIsMySALTVALUE";
 		System.out.println(DigestUtils.md5DigestAsHex(salted.getBytes()));
+	}
+	
+	@Test
+	void testNoOp() {
+		PasswordEncoder noOp = NoOpPasswordEncoder.getInstance();
+		System.out.println(noOp.encode(PASSWORD));
+		assertEquals(noOp.encode(PASSWORD), PASSWORD);
 	}
 }

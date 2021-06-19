f21c3c54ab87a68f637561c76a63ea60c41f5878
amarchino <marchino.alessandro@gmail.com>
2021-06-19 23:28:39 +0200

    Lesson 50

---
+++ 01-brewery/src/main/java/guru/sfg/brewery/domain/security/User.java
@@ -0,0 +1,33 @@
+package guru.sfg.brewery.domain.security;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	private String username;
+	private String password;
+	@ManyToMany(cascade = CascadeType.MERGE)
+	@JoinTable(name = "user_authority",
+		joinColumns = @JoinColumn(name = "USER_ID", referencedColumnName = "ID"),
+		inverseJoinColumns = @JoinColumn(name = "AUTHORITY_ID", referencedColumnName = "ID"))
+	private Set<Authority> authorities;
+
+	private Boolean accountNonExpired = Boolean.TRUE;
+	private Boolean accountNonLocked = Boolean.TRUE;
+	private Boolean credentialsNonExpired = Boolean.TRUE;
+	private Boolean enabled = Boolean.TRUE;
+
+}

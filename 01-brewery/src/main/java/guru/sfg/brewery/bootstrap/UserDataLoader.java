43369c2c4c97df50531f96cc877b5d9326d04a5d
amarchino <marchino.alessandro@gmail.com>
2021-06-21 22:47:35 +0200

    Lesson 68

--- 01-brewery/src/main/java/guru/sfg/brewery/bootstrap/UserDataLoader.java
+++ 01-brewery/src/main/java/guru/sfg/brewery/bootstrap/UserDataLoader.java
@@ -3,6 +3,8 @@ package guru.sfg.brewery.bootstrap;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,9 +12,10 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import guru.sfg.brewery.domain.security.Authority;
+import guru.sfg.brewery.domain.security.Role;
 import guru.sfg.brewery.domain.security.User;
-import guru.sfg.brewery.domain.security.User.UserBuilder;
 import guru.sfg.brewery.repositories.security.AuthorityRepository;
+import guru.sfg.brewery.repositories.security.RoleRepository;
 import guru.sfg.brewery.repositories.security.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,55 +27,99 @@ public class UserDataLoader implements CommandLineRunner {
 	
 	private final UserRepository userRepository;
 	private final AuthorityRepository authorityRepository;
+	private final RoleRepository roleRepository;
 	private final PasswordEncoder passwordEncoder;
 	
-	private Map<String, Authority> autorities = new HashMap<>();
+	private Map<ValuedEnum, Authority> autorities = new HashMap<>();
+	private Map<ValuedEnum, Role> roles = new HashMap<>();
 	
 	@Override
 	@Transactional
     public void run(String... args) {
-		addAuthorities();
+		// Authorities
+		Stream.of(BeerAuthorities.values()).forEach(this::addAuthority);
 		log.debug("Authorities loaded: " + authorityRepository.count());
-		addUsers();
+		
+		// Roles
+		addRole(Roles.ADMIN, BeerAuthorities.CREATE, BeerAuthorities.READ, BeerAuthorities.UPDATE, BeerAuthorities.DELETE);
+		addRole(Roles.CUSTOMER, BeerAuthorities.READ);
+		addRole(Roles.USER, BeerAuthorities.READ);
+		log.debug("Roles loaded: " + roleRepository.count());
+		
+		// Users
+		addUser("spring", "guru", Roles.ADMIN);
+		addUser("user", "password", Roles.USER);
+		addUser("scott", "tiger", Roles.CUSTOMER);
 		log.debug("Users loaded: " + userRepository.count());
     }
 
-	private void addAuthorities() {
-		autorities.put("ADMIN", addAuthority("ROLE_ADMIN"));
-		autorities.put("USER", addAuthority("ROLE_USER"));
-		autorities.put("CUSTOMER", addAuthority("ROLE_CUSTOMER"));
-	}
 	
-	private Authority addAuthority(String role) {
-		Optional<Authority> savedAuthority = authorityRepository.findByRole(role);
+	private Authority addAuthority(ValuedEnum authorityEnum) {
+		Optional<Authority> savedAuthority = authorityRepository.findByPermission(authorityEnum.value());
 		if(savedAuthority.isPresent()) {
-			log.debug("Role " + role + " already present");
+			log.debug("Authority " + authorityEnum + " already present");
 			return savedAuthority.get();
 		}
-		log.debug("Role " + role + " created");
-		return authorityRepository.save(Authority.builder().permission(role).build());
+		log.debug("Authority " + authorityEnum + " created");
+		Authority authority = authorityRepository.save(Authority.builder().permission(authorityEnum.value()).build());
+		autorities.put(authorityEnum, authority);
+		return authority;
+	}
+	
+	private Role addRole(ValuedEnum roleEnum, ValuedEnum... authorityValues) {
+		Optional<Role> savedRole = roleRepository.findByName(roleEnum.value());
+		if(savedRole.isPresent()) {
+			log.debug("Role " + roleEnum + " already present");
+			return savedRole.get();
+		}
+		log.debug("Role " + roleEnum + " created");
+		Role role = roleRepository.save(
+				Role.builder()
+				.name(roleEnum.value())
+				.authorities(Stream.of(authorityValues).map(autorities::get).collect(Collectors.toSet()))
+				.build());
+		roles.put(roleEnum, role);
+		return role;
 	}
 
-	private void addUsers() {
-		addUser("spring", "guru", "ADMIN");
-		addUser("user", "password", "USER");
-		addUser("scott", "tiger", "CUSTOMER");
-	}
-
-	private User addUser(String username, String password, String... roles) {
+	private User addUser(String username, String password, ValuedEnum... roleValues) {
 		Optional<User> savedUser = userRepository.findByUsername(username);
 		if(savedUser.isPresent()) {
 			log.debug("User " + username + " already present");
 			return savedUser.get();
 		}
 		log.debug("User " + username + " created");
-		UserBuilder builder = User.builder()
+
+		return userRepository.save(User.builder()
 			.username(username)
-			.password(passwordEncoder.encode(password));
-		for(String role : roles) {
-//			builder = builder.authority(autorities.get(role));
+			.password(passwordEncoder.encode(password))
+			.roles(Stream.of(roleValues).map(roles::get).collect(Collectors.toSet()))
+			.build());
+	}
+	
+	private static interface ValuedEnum {
+		String value();
+	}
+	private static enum BeerAuthorities implements ValuedEnum {
+		CREATE, UPDATE, READ, DELETE;
+		@Override
+		public String value() {
+			return "beer." + name().toLowerCase();
 		}
-		
-		return userRepository.save(builder.build());
+		@Override
+		public String toString() {
+			return getClass().getSimpleName() + "." + name();
+		}
+	}
+	private static enum Roles implements ValuedEnum {
+		ADMIN, CUSTOMER, USER;
+		@Override
+		public String value() {
+			return name();
+		}
+		@Override
+		public String toString() {
+			return getClass().getSimpleName() + "." + name();
+		}
 	}
 }
--- 01-brewery/src/main/java/guru/sfg/brewery/repositories/security/RoleRepository.java
+++ 01-brewery/src/main/java/guru/sfg/brewery/repositories/security/RoleRepository.java
@@ -1,9 +1,12 @@
 package guru.sfg.brewery.repositories.security;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import guru.sfg.brewery.domain.security.Role;
 
 public interface RoleRepository extends JpaRepository<Role, Integer> {
 
+	Optional<Role> findByName(String name);
 }

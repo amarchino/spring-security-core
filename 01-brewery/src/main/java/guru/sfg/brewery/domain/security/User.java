0cfc7792f3df155359888fb6cd617fc59fd877e4
amarchino <marchino.alessandro@gmail.com>
2021-06-22 17:14:33 +0200

    Lesson 75

--- 01-brewery/src/main/java/guru/sfg/brewery/domain/security/User.java
+++ 01-brewery/src/main/java/guru/sfg/brewery/domain/security/User.java
@@ -13,6 +13,11 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import org.springframework.security.core.CredentialsContainer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -26,8 +31,10 @@ import lombok.Singular;
 @Getter
 @Setter
 @Builder
-public class User {
+public class User implements UserDetails, CredentialsContainer {
 	
+	private static final long serialVersionUID = 1528485524463947681L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
@@ -50,12 +57,38 @@ public class User {
 	@Builder.Default
 	private Boolean enabled = Boolean.TRUE;
 	
-	public Set<Authority> getAuthorities() {
+	public Set<GrantedAuthority> getAuthorities() {
 		return this.roles
 			.stream()
 			.map(Role::getAuthorities)
 			.flatMap(Set::stream)
+			.map(a -> new SimpleGrantedAuthority(a.getPermission()))
 			.collect(Collectors.toSet());
+	}
+
+	@Override
+	public void eraseCredentials() {
+		password = null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return Boolean.TRUE.equals(accountNonExpired);
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return Boolean.TRUE.equals(accountNonLocked);
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return Boolean.TRUE.equals(credentialsNonExpired);
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return Boolean.TRUE.equals(enabled);
 	}
 
 }
--- 01-brewery/src/main/java/guru/sfg/brewery/security/JpaUserDetailsService.java
+++ 01-brewery/src/main/java/guru/sfg/brewery/security/JpaUserDetailsService.java
@@ -1,18 +1,11 @@
 package guru.sfg.brewery.security;
 
-import java.util.Collections;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import guru.sfg.brewery.domain.security.Authority;
-import guru.sfg.brewery.domain.security.User;
 import guru.sfg.brewery.repositories.security.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -27,22 +20,8 @@ public class JpaUserDetailsService implements UserDetailsService {
 	@Transactional(readOnly = true)
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		log.debug("Setting User info via JPA");
-		User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User name: " + username + " not found"));
-		Set<Authority> authorities = user.getAuthorities();
-		return new org.springframework.security.core.userdetails.User(
-				user.getUsername(),
-				user.getPassword(),
-				Boolean.TRUE.equals(user.getEnabled()),
-				Boolean.TRUE.equals(user.getAccountNonExpired()),
-				Boolean.TRUE.equals(user.getCredentialsNonExpired()),
-				Boolean.TRUE.equals(user.getAccountNonLocked()),
-				authorities != null && !authorities.isEmpty()
-					? authorities
-						.stream()
-						.map(Authority::getPermission)
-						.map(SimpleGrantedAuthority::new)
-						.collect(Collectors.toSet())
-					: Collections.emptySet());
+		return userRepository.findByUsername(username)
+			.orElseThrow(() -> new UsernameNotFoundException("User name: " + username + " not found"));
 	}
 	
 }

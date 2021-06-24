0bffc73e8ce66702030256b333d711374f0f86f0
amarchino <marchino.alessandro@gmail.com>
2021-06-24 13:21:14 +0200

    Lesson 102

--- 01-brewery/src/main/java/guru/sfg/brewery/security/listener/AuthenticationSuccessListener.java
+++ 01-brewery/src/main/java/guru/sfg/brewery/security/listener/AuthenticationSuccessListener.java
@@ -2,8 +2,11 @@ package guru.sfg.brewery.security.listener;
 
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 
+import guru.sfg.brewery.domain.security.User;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -12,5 +15,14 @@ public class AuthenticationSuccessListener {
 	@EventListener
 	public void listen(AuthenticationSuccessEvent event) {
 		log.debug("User logged in ok");
+		Authentication authentication = event.getAuthentication();
+		if(authentication.getPrincipal() instanceof User) {
+			User user = (User) authentication.getPrincipal();
+			log.debug("User name logged in: " + user.getUsername());
+		}
+		if(authentication.getDetails() instanceof WebAuthenticationDetails) {
+			WebAuthenticationDetails details = (WebAuthenticationDetails) authentication.getDetails();
+			log.debug("Source IP: " + details.getRemoteAddress());
+		}
 	}
 }
